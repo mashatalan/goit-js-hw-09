f172127790d9e6ec994a2e2fc40288240fea7a91
Maria Talan <mashenka.talan@gmail.com>
2023-05-07 20:01:37 +0300

    Completed color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  btnStart: document.querySelector('#start-btn'),
+  btnStop: document.querySelector('#stop-btn'),
+  body: document.body,
+};
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let intervalId = null;
+
+function startChangingBackgroundColor() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
+
+function stopChangingBackgroundColor() {
+  clearInterval(intervalId);
+  intervalId = null;
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
+
+refs.btnStart.addEventListener('click', startChangingBackgroundColor);
+refs.btnStop.addEventListener('click', stopChangingBackgroundColor);
